365dac0ff34d670639316e55caa2a0cad09b941b
Utkarsh.Raghav <Utkarsh.Raghav@canberra.edu.au>
2022-11-19 17:25:50 +1100

    Refactored and added wav generation function.

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ import morse_code
 
 
 def main():
-    message = "I LOVE YOU"
+    message = "HELLO HELLO"
     result = morse_code.encode(message)
     print(result)
-    morse_code.play_morse_code(message)
+    # morse_code.play_morse_code(message)
+    morse_code.generate_morse_code_wav(message)
 
 
 if __name__ == '__main__':
--- morse_code.py
+++ morse_code.py
@@ -1,6 +1,6 @@
 from playsound import playsound
 from time import sleep
-
+from pydub import AudioSegment
 # Dictionary representing the international morse code
 MORSE_CODE_DICT = {'A': '.-', 'B': '-...',
                    'C': '-.-.', 'D': '-..', 'E': '.',
@@ -21,7 +21,7 @@ REVERSE_MORSE_CODE_DICT = {v: k for k, v in MORSE_CODE_DICT.items()}
 
 def encode(message):
     cipher = ''
-    for letter in message:
+    for letter in message.upper():
         if letter.isalnum():
             cipher += MORSE_CODE_DICT[letter] + ' '
         else:
@@ -39,10 +39,27 @@ def decode(message):
 def play_morse_code(message):
     for character in encode(message.upper()):
         if character == '.':
-            playsound('dit.wav')
+            playsound('sounds/dit.wav')
             sleep(0.2)
         if character == '-':
-            playsound('dah.wav')
+            playsound('sounds/dah.wav')
             sleep(0.2)
         if character == ' ' or character == '/':
             sleep(0.32)
+
+
+def generate_morse_code_wav(message):
+    dah = AudioSegment.from_wav('sounds/dah.wav')
+    dit = AudioSegment.from_wav('sounds/dit.wav')
+    space_05 = AudioSegment.from_wav('sounds/silent_quarter-second.wav')
+    space_10 = AudioSegment.from_wav('sounds/silent_half-second.wav')
+
+    combined = AudioSegment.empty()
+    for character in encode(message.upper()):
+        if character == '.':
+            combined += (dit + space_05)
+        if character == '-':
+            combined += (dah + space_05)
+        if character == ' ' or character == '/':
+            combined += space_10
+    combined.export("output.wav", format="wav")
